af9099410b78c982c9ae4d932793f2c27a367b50
davidagina <davidagina64@gmail.com>
2022-04-03 18:10:41 +0100

    php for form

--- mail.php
+++ mail.php
@@ -1,13 +1,14 @@
 <?php
 //get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
+$name = $_GET['name'];
+$email= $_GET['email'];
+$message= $_GET['message'];
 $to = "david-agina@outlook.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: noreply@davidagina.github.io" . "\r\n" .
+"CC: ;
+
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
